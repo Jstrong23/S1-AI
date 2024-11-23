8708d212b934e9bc1cac5899ce448155f172c25a
Jstrong23 <joshua.strong23@bathspa.ac.uk>
2024-11-23 18:12:41 +0000

    g

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ y = iris.data.targets
   
 # metadata 
 print(iris.metadata) 
-  g
+  
 # variable information 
 print(iris.variables) 
 #---------------------------------------------This code was created to import the iris dataset (Fisher, 1936) into python by (UCI Machine Learning Repository,https://archive.ics.uci.edu/dataset/53/iris)
