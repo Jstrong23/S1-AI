2a630e31e2c2db0ee7b5a8ebeed7154ffb1d5593
Jstrong23 <joshua.strong23@bathspa.ac.uk>
2024-11-24 14:29:09 +0000

    change to make the AI only classify 2 types of plant

--- random forest.py
+++ random forest.py
@@ -5,46 +5,38 @@ import matplotlib.pyplot as plt
 
 #create AI class
 class AI:
-    def decision(self, test_data, SetosaMeans, versicolourMeans, virginicaMeans):
+    def decision(self, test_data, SetosaMeans, virginicaMeans):
         setosa_score = 0
         versicolour_score = 0
-        virginica_score = 0
-        total_score = 0
-        if (test_data.values[0] > SetosaMeans.values[0] - 0.5) and (test_data.values[0] < SetosaMeans.values[0] + 0.5):
-            setosa_score += 1
-        if (test_data.values[1] > SetosaMeans.values[1] - 0.5) and (test_data.values[1] < SetosaMeans.values[1] + 0.5):
-            setosa_score += 1
-        if (test_data.values[2] > SetosaMeans.values[2] - 0.5) and (test_data.values[2] < SetosaMeans.values[2] + 0.5):
-            setosa_score += 1
-        if (test_data.values[3] > SetosaMeans.values[3] - 0.5) and (test_data.values[3] < SetosaMeans.values[3] + 0.5):
-            setosa_score += 1
-        if (test_data.values[0] > versicolourMeans.values[0] - 0.5) and (test_data.values[0] < versicolourMeans.values[0] + 0.5):
-            versicolour_score += 1
-        if (test_data.values[1] > versicolourMeans.values[1] - 0.5) and (test_data.values[1] < versicolourMeans.values[1] + 0.5):
-            versicolour_score += 1
-        if (test_data.values[2] > versicolourMeans.values[2] - 0.5) and (test_data.values[2] < versicolourMeans.values[2] + 0.5):
-            versicolour_score += 1
-        if (test_data.values[3] > versicolourMeans.values[3] - 0.5) and (test_data.values[3] < versicolourMeans.values[3] + 0.5):
-            versicolour_score += 1
-        if (test_data.values[0] > virginicaMeans.values[0] - 0.5) and (test_data.values[0] < virginicaMeans.values[0] + 0.5):
-            virginica_score += 1
-        if (test_data.values[1] > virginicaMeans.values[1] - 0.5) and (test_data.values[1] < virginicaMeans.values[1] + 0.5):
-            virginica_score += 1
-        if (test_data.values[2] > virginicaMeans.values[2] - 0.5) and (test_data.values[2] < virginicaMeans.values[2] + 0.5):
-            virginica_score += 1
-        if (test_data.values[3] > virginicaMeans.values[3] - 0.5) and (test_data.values[3] < virginicaMeans.values[3] + 0.5):
-            virginica_score += 1
         
-        total_score = setosa_score + versicolour_score + virginica_score
+        total_score = 0
+        if (test_data.values[0] > SetosaMeans.values[0] - 0.3) and (test_data.values[0] < SetosaMeans.values[0] + 0.3):
+            setosa_score += 1
+        if (test_data.values[1] > SetosaMeans.values[1] - 0.3) and (test_data.values[1] < SetosaMeans.values[1] + 0.3):
+            setosa_score += 1
+        if (test_data.values[2] > SetosaMeans.values[2] - 0.3) and (test_data.values[2] < SetosaMeans.values[2] + 0.3):
+            setosa_score += 1
+        if (test_data.values[3] > SetosaMeans.values[3] - 0.3) and (test_data.values[3] < SetosaMeans.values[3] + 0.3):
+            setosa_score += 1
+        if (test_data.values[0] > versicolourMeans.values[0] - 0.3) and (test_data.values[0] < versicolourMeans.values[0] + 0.3):
+            versicolour_score += 1
+        if (test_data.values[1] > versicolourMeans.values[1] - 0.3) and (test_data.values[1] < versicolourMeans.values[1] + 0.3):
+            versicolour_score += 1
+        if (test_data.values[2] > versicolourMeans.values[2] - 0.3) and (test_data.values[2] < versicolourMeans.values[2] + 0.3):
+            versicolour_score += 1
+        if (test_data.values[3] > versicolourMeans.values[3] - 0.3) and (test_data.values[3] < versicolourMeans.values[3] + 0.3):
+            versicolour_score += 1
+        
+        #calculate probability of each plant
+        setosa_prob = setosa_score/4
+        versicolour_prob = versicolour_score/4
 
-        if setosa_score > versicolour_score and setosa_score > virginica_score:
-            return("setosa", int(setosa_score / total_score * 100))
-        elif virginica_score > setosa_score and virginica_score > versicolour_score:
-            return("virginica", int(virginica_score / total_score * 100))
-        elif versicolour_score > setosa_score and versicolour_score > virginica_score:
-            return("versicolour", int(versicolour_score / total_score * 100))
+        if setosa_prob > 0.3:
+            return("setosa", setosa_prob)
+        elif versicolour_prob > 0.3:
+            return("versicolour", versicolour_prob)
         else:
-            return("undetermined", 0)
+            return("undetermined", 1 - setosa_prob - versicolour_prob)
                 
 #create instance of AI
 ai = AI()
@@ -57,15 +49,15 @@ from ucimlrepo import fetch_ucirepo
 iris = fetch_ucirepo(id=53) 
   
 # data (as pandas dataframes) 
-X = iris.data.features 
-y = iris.data.targets 
+#X = iris.data.features 
+#y = iris.data.targets 
   
 # metadata 
 #print(iris.metadata) 
   
 # variable information 
 #print(iris.variables) 
-#---------------------------------------------This code was created to import the iris dataset (Fisher, 1936) into python by (UCI Machine Learning Repository,https://archive.ics.uci.edu/dataset/53/iris)
+#---------------------------------------------This code was created to import the iris dataset (Fisher, 1936) into python by (UCI Machine Learning Repository, https://archive.ics.uci.edu/dataset/53/iris)
 
 #choose training data
 data = iris.data.features
@@ -76,13 +68,13 @@ df_Target = pd.DataFrame(species)
 #chose training data
 Setosa_train = df.loc[0:34]
 versicolour_train = df.loc[50:74]
-virginica_train = df.loc[100:134]
+
 
 
 #calculate means of training data
 SetosaMeans = Setosa_train.mean()
 versicolourMeans = versicolour_train.mean()
-virginicaMeans = virginica_train.mean()
+
 #print(SetosaMeans)
 #print(versicolourMeans)
 #print(virginicaMeans)
@@ -93,10 +85,15 @@ virginicaMeans = virginica_train.mean()
 setosa_test = df.loc[35:49]
 versicolour_test = df.loc[75:99]
 virginica_test = df.loc[135:149]
+
+#prep performance data
 FP = 0
 FN = 0
 TP = 0
 TN = 0
+true_array = []
+score_array = []
+
 for x in range (45):
     prob = 0
     test = "setosa"
@@ -110,20 +107,29 @@ for x in range (45):
             test = "virginica"
             i += 35
             test_data = virginica_test
-    output, prob = ai.decision(test_data.loc[i], SetosaMeans, versicolourMeans, virginicaMeans)
+    output, prob = ai.decision(test_data.loc[i], SetosaMeans, versicolourMeans)
     if output == test:
-        correct = "Correct"
+        correct = "true"
+        TP += 1
+        true_array.append(1)
+    elif output == "undetermined" and test == "virginica":
+        correct = "true"
+        TN += 1
+        true_array.append(1)
+    elif output == "undetermined":
+        correct = "false"
+        FN += 1
+        true_array.append(0)
     else:
         correct = "false"
-    print (x, "---", output, "---",prob, "%", "---", correct)
-
-    #calcualate true positives, false positives, trua negatives and false negatives for performance metrics
-    if output == test:
-        TP += 1
-    elif output == "undetermined":
-        FN += 1
-    else:
         FP += 1
+        true_array.append(0)
+
+    score_array.append(prob)
+    print (x, "---", output, "---", prob, "---", correct)
+
+    
+    
     
 #accuracy
 accuracy = (TP+FN)/(TP+TN+FP+FN)
@@ -141,11 +147,17 @@ print (recall)
 F1Score = 2*((precision*recall)/(precision+recall))
 print (F1Score)
 
+
+
 #PRC
-PR_auc = auc(recall, precision)
+# the following code has been taken from the "Session-4-Rule-Based-AI-Systems" powerpoint (Bath Spa University, "https://minerva.bathspa.ac.uk/ultra/courses/_137310_1/outline/file/_2168805_1")
+recall_array = [0.23333333333333334, 0.23333333333333334, 0.7666666666666667, 0.7666666666666667, 0.7666666666666667, 0.9333333333333333, 1.0, 1.0]
+precision_array = [1.0, 1.0, 1.0, 1.0, 1.0, 0.875, 0.6585365853658537, 0.6585365853658537]
+
+PR_auc = auc(recall_array, precision_array)
 
 plt.figure()
-plt.plot(recall, precision, color="b", lw=1, label='pr curve (area = %0.2f)' % PR_auc)
+plt.plot(precision, precision, color="b", lw=1, label='pr curve (area = %0.2f)' % PR_auc)
 plt.xlabel('recall')
 plt.ylabel('precision')
 plt.title('Precison Recall curve')
